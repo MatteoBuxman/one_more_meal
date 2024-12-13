d08a266c52d80e483c4a09c5934d0b38b667ac19
MatteoBuxman <buxmanm@icloud.com>
2024-12-13 22:16:27 +0200

    Created an initial landing page and logo, and experimented with some colours.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,31 @@
 import containerQueries from '@tailwindcss/container-queries';
 import type { Config } from 'tailwindcss';
+declare function require(name:string):any;
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+  daisyui: {
+    darkTheme: "light",
+   },
 
-  plugins: [containerQueries]
+  theme: {
+    extend: {
+      colors: {
+        'smoky-black': '#0F1108', // Custom name for 0F1108
+        'yellow-green': '#B0DB43', // Custom name for B0DB43
+        'electric-indigo': '#5E2BFF', // Custom name for 5E2BFF
+        'carolina-blue': '#86BBD8', // Custom name for 86BBD8
+        'light-cyan': '#D7F9FF', // Custom name for D7F9FF
+      },
+      fontFamily:{
+        lexend: ['Lexend', 'sans-serif'],
+      }
+  },
+},
+
+  plugins: [
+    containerQueries,
+    require('daisyui'),
+  ]
 } satisfies Config;
